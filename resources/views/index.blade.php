c4b42f1323e5f857c6b1d22ee4b19cccf29b6d6a
YusukeOno <o.yusuke.od@gmail.com>
2017-04-17 23:24:55 +0900

    matealize hello

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+        <!-- Compiled and minified CSS -->
+        <link href="../../materialize/css/materialize.min.css" "stylesheet" type="text/css">
+        <!-- Compiled and minified JavaScript -->
+        <script src="../../materialize/js/materialize.min.js" type="text/css"></script>
+    </head>
+    <body>
+        <h1>HelloWord</h1>
+    </body>
+</html>
